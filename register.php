4c50878668fea1ffbc4d5b28db80b00606e226ef
Iqbal Fadhil <iqbal.fadhil.3795@gmail.com>
2020-05-31 00:23:32 +0700

    first commit

---
+++ register.php
@@ -0,0 +1,27 @@
+<?php
+/* Attempt MySQL server connection. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+$link = mysqli_connect("localhost", "root", "", "getpath_lp_db");
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+ 
+// Escape user inputs for security
+$full_name = mysqli_real_escape_string($link, $_REQUEST['full_name']);
+$email = mysqli_real_escape_string($link, $_REQUEST['email']);
+$whatsapp = mysqli_real_escape_string($link, $_REQUEST['whatsapp']);
+ 
+// Attempt insert query execution
+$sql = "INSERT INTO registers (full_name, email, whatsapp) VALUES ('$full_name', '$email', '$whatsapp')";
+if(mysqli_query($link, $sql))
+    {readfile("thankyou_registration.html");
+} 
+else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+ 
+// Close connection
+mysqli_close($link);
+?>
